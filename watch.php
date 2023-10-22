97d65636bc0c4076762226745d00ecae883dd394
portalplayer <portalplayer@crime.su>
2023-10-22 01:07:44 -0400

    watch: Detect invalid player and video ids

--- watch.php
+++ watch.php
@@ -9,6 +9,17 @@ if (!isset($_GET["v"]) || !isset($_GET["p"])) {
 $id = htmlspecialchars($_GET["v"]);
 $version = htmlspecialchars($_GET["p"]);
 $directory = getcwd();
+
+if (!file_exists("$directory/players/player_$version.swf")) {
+    header("Location: /");
+    die("Invalid player version: <a href='/'>Return</a>");
+}
+
+if (!file_exists("$directory/videos/$id.flv")) {
+    header("Location: /");
+    die("Invalid video ID: <a href='/'>Return</a>");
+}
+
 $length = \FFMpeg\FFProbe::create()->format("$directory/videos/$id.flv")->get("duration");
 $source = "/players/player_$version.swf?video_id=$id&l=$length&t=$length";
 ?>
